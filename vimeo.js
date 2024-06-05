d9f41f0ca5fab0b231b2be90c9153996fdca4c12
IIIgorMoura <133662971+IIIgorMoura@users.noreply.github.com>
2024-06-05 16:48:58 -0300

    base desafios

---
+++ vimeo.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_KEY_VIMEO = '4273ef75cb4b9129a4d47e7cdf0b2ef855ae7710';
+
+export const buscarVideosVimeo = async (query) => {
+  try {
+    const pesquisa = await axios.get('https://api.vimeo.com/videos', {
+      params: {
+        q: query,
+        part: 'snippet',
+        maxResults: 10,
+        type: 'video',
+        key: API_KEY_VIMEO,
+      },
+    });
+    return pesquisa.data.items;
+  } catch (erro) {
+    console.error('Erro ao buscar vídeos do YouTube:', erro);
+    throw erro;
+  }
+};
